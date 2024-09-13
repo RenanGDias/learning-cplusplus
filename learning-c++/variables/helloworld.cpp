14e722ba975bb6f3b60bcba269223e38d4410560
Renan Dias <129848338+RenanGDias@users.noreply.github.com>
2024-09-13 20:43:28 -0300

    Learned Variables
    Learned the variables Integer, Double, Char, Boolean and String

---
+++ learning-c++/variables/helloworld.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+int main()
+{
+
+    /*
+    int x; //declaration
+    x = 5; //assignment
+
+    int x = 5; // declaration and assignment in the same line!
+    int y = 6;
+    int sum = x + y;
+
+    std::cout << x << '\n';
+    std::cout << y << '\n';
+    std::cout << sum << '\n';
+
+    */
+
+    int age = 21;
+    int year = 2023;
+    int days = 7.5;
+
+    //double (number including decimal)
+    double price = 10.99;
+    double gpa = 2.5;
+    double temperature = 25.1;
+
+    // single character
+    char grade = 'A';
+    char initial = 'C';
+    char currency = '$';
+
+    // boolean (true or false)
+    bool student = false;
+    bool power = true;
+    bool forSale = true;
+
+    // string (objects that represents a sequence of text)
+    std::string name = "Bro";
+    std::string day = "Friday";
+    std::string food = "pizza";
+    std::string address = "123 Fake St.";
+
+    std::cout << "Hello " << name << '\n';
+    std::cout << "You are " << age << " years old";
+
+    return 0;
+}
